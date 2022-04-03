76dc406d8dbd2502fea6ddf18bb6cbf91ec9b34e
Sébastien Gatty <sebastien.gatty@homoludens.games>
2022-04-03 22:41:58 +0200

    Multiple videos supported in title screen

--- Menu/TitleScreen.gd
+++ Menu/TitleScreen.gd
@@ -1,8 +1,9 @@
 extends Control
 
 
-export (float) var fade_time : float = 3.0
+export (float) var fade_time : float = 2.0
 export (float) var video_duration : float = 10.0
+export (Array, Resource) var videos : Array
 
 
 onready var video_player : VideoPlayer = $VideoPlayer
@@ -14,6 +15,7 @@ onready var hide_color : Color = fader.color
 
 var transparent : Color
 var is_hidden := true
+var video_index := 0
 
 
 func _ready() -> void:
@@ -27,8 +29,11 @@ func _process(_delta: float) -> void:
 
 
 func play_video() ->  void:
-	video_player.play()
-	fade_timer.start(1)
+	if videos.size() > 0:
+		video_player.stream = videos[video_index]
+		video_player.play()
+		fade_timer.start(1)
+		video_index = (video_index + 1) % videos.size()
 
 
 func fade_in() -> void:
